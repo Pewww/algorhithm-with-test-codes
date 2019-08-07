0ebd174a2d847792dff64fce6c0b0fbf071123fe
Pewww <jung22337@naver.com>
2019-08-08 00:10:03 +0900

    :sparkles: 더욱 단순한 버전의 gcd 함수 및 테스트 코드 추가

--- recursion/gcd.js
+++ recursion/gcd.js
@@ -3,9 +3,14 @@
  * 
  * m >= n인 두 양의 정수 m과 n에 대해서 m이 n의 배수이면 gcd(m, n) = n이고,
  * 그렇지 않으면 gcd(m, n) = gcd(n, m % n)이다.
+ * 
+ * 더욱 단순한 버전 (m이 n보다 크지 않아도 된다.)
+ *               m               if n = 0
+ *  gcd(m, n) =>
+ *               gcd(n, m % n)   otherwise.
  */
 
-export default function gcd(m, n) {
+export const gcd = (m, n) => {
   if (m < n) {
     let temp = m;
     m = n;
@@ -17,4 +22,12 @@ export default function gcd(m, n) {
   }
 
   return gcd(n, m % n);
-}
+} 
+
+export const simpleGcd = (m, n) => {
+  if (n === 0) {
+    return m;
+  }
+
+  return simpleGcd(n, m % n);
+};
--- recursion/test/gcd.test.js
+++ recursion/test/gcd.test.js
@@ -1,23 +1,28 @@
-import gcd from '../gcd';
+import {gcd, simpleGcd} from '../gcd';
 
-describe(`Verity that the function 'gcd' returns the greatest common measure.`, () => {
+describe(`Verity that the function 'gcd' and 'simpleGcd' returns the greatest common measure.`, () => {
   it('It should return 3 if m = 6, n = 3', () => {
     expect(gcd(6, 3)).toBe(3);
+    expect(simpleGcd(6, 3)).toBe(3);
   });
 
   it('It should return 11 if m = 143, n = 11', () => {
     expect(gcd(143, 11)).toBe(11);
+    expect(simpleGcd(143, 11)).toBe(11);
   });
 
   it('It should return 5 if m = 45, n = 55', () => {
     expect(gcd(45, 55)).toBe(5);
+    expect(simpleGcd(45, 55)).toBe(5);
   });
 
   it('It should return 1 if m = 12, n = 17', () => {
     expect(gcd(12, 17)).toBe(1);
+    expect(simpleGcd(12, 17)).toBe(1);
   });
 
   it('It should return 10 if m = 10, n = 50', () => {
     expect(gcd(10, 50)).toBe(10);
+    expect(simpleGcd(10, 50)).toBe(10);
   });
 });
